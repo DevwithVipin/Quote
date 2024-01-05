f80be43f3bdea65bd51415b027fc42d746f247c4
codermonkvipin <codermonkvipin@gmail.com>
2024-01-05 22:44:27 +0530

    added remove and add to bookmark features

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -1,38 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { update } from '../utils/bookMarkSlice';
 import BookmarkQuote from './BookmarkQuote';
 
 function Bookmark() {
   const dispatch = useDispatch();
-  const bookmarks = useSelector((store) => store.bookmark);
-  console.log(bookmarks);
-
+  const [bookmarks,setBookmarks]=useState('');
   useEffect(() => {
     const existingBookmarks = localStorage.getItem('bookmarks');
     if (existingBookmarks) {
-      try {
-        const parsedBookmarks = JSON.parse(existingBookmarks);
-        if (Array.isArray(parsedBookmarks)) {
-          dispatch(update(parsedBookmarks));
-        } else {
-          dispatch(update([])); // Initialize with an empty array if the parsed data isn't an array
-        }
-      } catch (error) {
-        console.error('Error parsing bookmarks:', error);
-        dispatch(update([])); // Initialize with an empty array if parsing fails
-      }
+      {const bookmarks = JSON.parse(existingBookmarks);
+      setBookmarks(bookmarks)
+    console.log(bookmarks)}
+
+      dispatch(update(bookmarks));
     }
   }, [dispatch]);
-
+  // const bookmarks = useSelector((store) => store.bookmark);
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-500 to-purple-500 relative">
-      { bookmarks.length > 0 ? (
+      
+      {bookmarks.length > 0 ? (
         bookmarks.map((bookmark, index) => (
-          <BookmarkQuote key={index} data={bookmark.data} author={bookmark.author} />
+          <BookmarkQuote
+            key={index}
+            data={bookmark.data}
+            author={bookmark.author}
+            index={index}
+          />
         ))
       ) : (
-        <p className='text-white'>No bookmarks available</p>
+        <p className="text-white">No bookmarks available</p>
       )}
     </div>
   );
